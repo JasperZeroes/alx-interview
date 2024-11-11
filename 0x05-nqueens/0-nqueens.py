5d465fa704c6730e14bd326e2af414286901d6d5
jasperzeroes <jasperobed@gmail.com>
2024-11-11 16:55:42 +0100

    a program that solves the N queens problem, in chess, by  placing N non-attacking queens on an N×N chessboard

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,59 +1,48 @@
 #!/usr/bin/python3
-"""N Queens"""
 import sys
 
-
-def print_board(board, n):
-    """Print allocated positions to the queen"""
-    b = []
-
-    for i in range(n):
-        for j in range(n):
-            if j == board[i]:
-                b.append([i, j])
-    print(b)
-
-
-def is_position_safe(board, i, j, r):
-    """Checks if the position is safe for the queen"""
-    return board[i] in (j, j - i + r, i - r + j)
-
-
-def safe_positions(board, row, n):
-    """Find all safe positions where the queen can be allocated"""
-    if row == n:
-        print_board(board, n)
-
-    else:
-        for j in range(n):
-            allowed = True
-            for i in range(row):
-                if is_position_safe(board, i, j, row):
-                    allowed = False
-            if allowed:
-                board[row] = j
-                safe_positions(board, row + 1, n)
-
-
-def create_board(size): #CREATE chess BOARD SIZE
-    """Generates the board"""
-    return [0 * size for i in range(size)]
-
-
-if len(sys.argv) != 2:
+def print_usage_and_exit():
     print("Usage: nqueens N")
-    exit(1)
+    sys.exit(1)
 
-try:
-    n = int(sys.argv[1])
-except BaseException:
-    print("N must be a number")
-    exit(1)
+def validate_input():
+    if len(sys.argv) != 2:
+        print_usage_and_exit()
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    return N
 
-if (n < 4):
-    print("N must be at least 4")
-    exit(1)
+def is_safe(board, row, col):
+    """Check if it's safe to place a queen at board[row][col]."""
+    for i in range(row):
+        if board[i] == col or \
+           board[i] - i == col - row or \
+           board[i] + i == col + row:
+            return False
+    return True
 
-board = create_board(int(n))
-row = 0
-safe_positions(board, row, int(n))
+def solve_nqueens(N):
+    """Solve the N-Queens problem and print all solutions."""
+    def backtrack(row):
+        if row == N:
+            solution = [[i, board[i]] for i in range(N)]
+            print(solution)
+            return
+        for col in range(N):
+            if is_safe(board, row, col):
+                board[row] = col
+                backtrack(row + 1)
+                board[row] = -1
+
+    board = [-1] * N
+    backtrack(0)
+
+if __name__ == "__main__":
+    N = validate_input()
+    solve_nqueens(N)
